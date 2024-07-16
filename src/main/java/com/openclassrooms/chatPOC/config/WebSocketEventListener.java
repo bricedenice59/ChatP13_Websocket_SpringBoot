ba1999eade9ea59137031d1584a1d6d4e0aea0b5
bricedenice59 <brice.grenard@gmail.com>
2024-07-16 11:58:56 +0200

    Completes websocket configuration.

--- src/main/java/com/openclassrooms/chatPOC/config/WebSocketEventListener.java
+++ src/main/java/com/openclassrooms/chatPOC/config/WebSocketEventListener.java
@@ -1,9 +1,14 @@
 package com.openclassrooms.chatPOC.config;
 
+import com.openclassrooms.chatPOC.chat.ChatMessage;
+import com.openclassrooms.chatPOC.chat.MessageType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionConnectEvent;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
 @Component
@@ -11,9 +16,25 @@ import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 @Slf4j
 public class WebSocketEventListener {
 
+    private final SimpMessageSendingOperations messageTemplate;
+
+    @EventListener
+    public void onWebSocketConnect(SessionConnectEvent event) {
+        log.info("Establishing WebSocket connection...");
+    }
+
+
     @EventListener
     public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
-        log.info("Received web socket disconnect event");
-
+        StompHeaderAccessor accessor = StompHeaderAccessor.wrap(event.getMessage());
+        String username = (String) accessor.getSessionAttributes().get("username");
+        if(username != null) {
+            log.info("Received web socket disconnect event:{}", username);
+            var chatMessage = ChatMessage.builder()
+                    .messageType(MessageType.LEAVE)
+                    .senderName(username)
+                    .build();
+            messageTemplate.convertAndSend("/topic/public", chatMessage);
+        }
     }
 }
