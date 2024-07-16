8a9d5b379562403e1dd2b6d378c545d6db452445
bricedenice59 <brice.grenard@gmail.com>
2024-07-16 11:26:28 +0200

    Minimal config for websocket configuration.

---
+++ src/main/java/com/openclassrooms/chatPOC/config/WebSocketEventListener.java
@@ -0,0 +1,19 @@
+package com.openclassrooms.chatPOC.config;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class WebSocketEventListener {
+
+    @EventListener
+    public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
+        log.info("Received web socket disconnect event");
+
+    }
+}
